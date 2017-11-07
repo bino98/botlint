83db310292c018d5bbe9ca33a399cc3b7a615c0e
Takafumi Yamamoto <takafumi.yamamoto@speee.jp>
2017-11-07 11:14:47 +0900

    change lint

--- src/botlint.js
+++ src/botlint.js
@@ -1,12 +1,13 @@
 import SlackBot from './bots/slack_bot'
-import English from './proofreads/english'
+// import English from './proofreads/english'
+import Japanese from './proofreads/japanese'
 import TextCheck from './bots/skills/text_check'
 
 class Botlint {
   constructor() {
     const slackBot = new SlackBot()
-    const english = new English()
-    new TextCheck(slackBot, english).set()
+    const japanese = new Japanese()
+    new TextCheck(slackBot, japanese).set()
   }
 }
 
